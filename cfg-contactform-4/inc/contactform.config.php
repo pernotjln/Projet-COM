293f6d98064895613eff7cd2ee89d3c7a1293945
pernotjln <pernotjln@gmail.com>
2013-11-13 21:20:20 +0100

    Base propre
    Import du projet sur GIT

---
+++ cfg-contactform-4/inc/contactform.config.php
@@ -0,0 +1,19 @@
+<?php
+$cfg['email_address'] = 'pernotjln@gmail.com';
+$cfg['email_from'] = '';
+$cfg['email_address_cc'] = '';
+$cfg['email_address_bcc'] = '';
+$cfg['timezone'] = 'Europe/Paris';
+$cfg['form_name'] = 'Mon formulaire de contact';
+$cfg['form_validationmessage'] = 'Merci, votre message m\'a bien été envoyé.<br />Je vous repondrez dès que possible.';
+$cfg['form_errormessage_captcha'] = '';
+$cfg['form_errormessage_emptyfield'] = '';
+$cfg['form_errormessage_invalidemailaddress'] = 'Addresse mail invalide';
+$cfg['form_redirecturl'] = '';
+$cfg['adminnotification_subject'] = 'Nouveau message envoyé de votre site';
+$cfg['usernotification_inputid'] = '';
+$cfg['usernotification_insertformdata'] = '';
+$cfg['usernotification_format'] = '';
+$cfg['usernotification_subject'] = '';
+$cfg['usernotification_message'] = '';
+?>
