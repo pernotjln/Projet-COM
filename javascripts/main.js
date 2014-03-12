a7119cde3c5e88e1a225bb4e4994ff0d297affa3
pernotjln <pernotjln@gmail.com>
2014-03-12 13:05:27 +0100

    Changement des icones de contact + css + contenu

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,11 @@
 
 $(document).ready(function(){
 	$("nav").sticky({topSpacing:0});
+	
+	$("a[rel^='prettyPhoto']").prettyPhoto({
+		social_tools: false,
+		theme: 'light_square'
+	});
 });
 
 function scrollTo(target){
@@ -14,12 +19,5 @@ $(window).load(function() {
 		slideshow: true,
 		slideshowSpeed: 4500,
 		animationSpeed: 1000
-	});
-});
-
-$(document).ready(function(){
-	$("a[rel^='prettyPhoto']").prettyPhoto({
-		social_tools: false,
-		theme: 'light_square'
 	});
 });
